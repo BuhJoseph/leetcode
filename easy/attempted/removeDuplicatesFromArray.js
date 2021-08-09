5c2d332040c34eb5b0dddd625e172e554a2a1e3a
Joseph Balaoing <joseph.balaoing1@yahoo.com>
2021-08-08 23:14:47 -0700

    Attempting Remove Duplicates From Sorted Array

---
+++ easy/attempted/removeDuplicatesFromArray.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  if (nums.length < 1) return 0;
+  var curr = nums[0];
+  var index = 1;
+  for (var i = 1; i < nums.length; i++) {
+    if (nums[i] !== curr) {
+      nums[index++] = nums[i];
+      curr = nums[i];
+    }
+  }
+  return index;
+};
