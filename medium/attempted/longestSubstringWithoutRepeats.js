a71e0e80706d7c9c2288e49c83c7f7e1fd3524df
Joseph Balaoing <joseph.balaoing1@yahoo.com>
2021-08-09 00:58:59 -0700

    Completed Longest Substring Without Repeating Characters but with poor performance

--- medium/attempted/longestSubstringWithoutRepeats.js
+++ medium/attempted/longestSubstringWithoutRepeats.js
@@ -5,5 +5,25 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
+  var longest = '';
+  var curr = '';
+  var chars = {};
+  for (var i = 0; i < s.length; i++) {
+    if (chars[s[i]] === undefined) {
+      chars[s[i]] = i;
+      curr = curr + s[i];
+    } else {
+      if (curr.length > longest.length) {
+        longest = curr;
+      }
+      i = chars[s[i]];
+      curr = '';
+      chars = {};
+    }
+  }
+  if (curr.length > longest.length) {
+    longest = curr;
+  }
 
+  return longest.length;
 };
