ba14ecc50dee3e1d8597ae876867850a461d366b
Joseph Balaoing <joseph.balaoing1@yahoo.com>
2021-08-09 01:22:43 -0700

    Improved solution by not needing to loop through chars object to delete indexes less than start

--- medium/attempted/longestSubstringWithoutRepeats.js
+++ medium/attempted/longestSubstringWithoutRepeats.js
@@ -1,7 +1,7 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
-// Runtime: Top 14%
-// Memory: Top 8%
+// Runtime: Top 46%
+// Memory: Top 56%
 
 /**
  * @param {string} s
@@ -12,18 +12,17 @@ var lengthOfLongestSubstring = function(s) {
   var start = 0;
   var chars = {};
   for (var i = 0; i < s.length; i++) {
-    console.log(longest);
-    if (chars[s[i]] === undefined) {
+    if (chars[s[i]] === undefined || chars[s[i]] < start) {
       chars[s[i]] = i;
     } else {
-      if (i - start - 1 > longest) {
+      if (i - start > longest) {
         longest = i - start;
       }
       start = chars[s[i]] + 1;
       chars[s[i]] = i;
     }
   }
-  console.log(start , i);
+
   if (i - start > longest) {
     longest = i - start;
   }
